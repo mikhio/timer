d812488ad2ff2e7a7cd973993d7f3cade4f2a1e2
Mikhail Iogansen <mikhail.iogansen@gmail.com>
2018-02-24 19:42:43 +0300

    secondmetter

--- index.js
+++ index.js
@@ -1,64 +1,84 @@
 var Timer = function(obj){
-  this.time = obj.time;
+  this.time = obj.startTime || 0;
+  this.toTime = obj.toTime || 99;
   this.interval = obj.interval || 1000;
-  this.onEnd = obj.onEnd || this.stop;
-  this.onStart = obj.onStart || null;
   this.onTick = obj.onTick || null;
   this.intervalID = null;
 
   this.start = () => {
+    if (this.intervalID) return
     this.intervalID = setInterval(this.update, this.interval);
   };
   this.stop = () => {
     clearInterval(this.intervalID);
+    this.intervalID = null;
   };
   this.update = () => {
-    this.time > 0 ? this.time -= 1 : this.onEnd();
+    this.time < this.toTime ? this.time += 1 : this.onEnd();
     this.onTick ? this.onTick() : void 0;
     return this.get();
-  }
+  };
+  this.reset = () => {
+    this.stop();
+    this.time = obj.startTime || 0;
+  };
   this.get = () => {
-    return this.time;
-  }
+    var tt = '' + this.time;
+    if (tt.length === 1) return '0' + tt;
+    return tt;
+  };
+
+  this.onEnd = obj.onEnd || this.stop;
 }
+
 var hour = new Timer({
-  time: 1,
   onTick: () => {
     id('hour').innerText = hour.get()
   }
 });
 
 var min = new Timer({
-  time: 60,
+  toTime: 59,
   onTick: () => {
     id('min').innerText = min.get()
   },
   onEnd: () => {
     hour.update();
-    min.time = 60;
+    min.reset();
   }
 });
 
 var sec = new Timer({
-  time: 60,
+  toTime: 59,
   interval: 1000,
   onTick: () => {
     id('sec').innerText = sec.get()
   },
   onEnd: () => {
     min.update();
-    sec.stop();
-    sec.time = 60;
-    sec.start();
+    sec.reset();
+  }
+});
+var ms = new Timer({
+  toTime: 99,
+  interval: 10,
+  onTick: () => {
+    id('ms').innerText = ms.get()
+  },
+  onEnd: () => {
+    sec.update();
+    ms.reset();
+    ms.start();
   }
 });
 
-id('start').addEventListener('click', sec.start);
-id('stop').addEventListener('click', sec.stop);
+id('start').addEventListener('click', ms.start);
+id('stop').addEventListener('click', ms.stop);
 
 requestAnimationFrame(hour.onTick)
 requestAnimationFrame(min.onTick)
 requestAnimationFrame(sec.onTick)
+requestAnimationFrame(ms.onTick)
 
 function id(id){
   return document.getElementById(id)
